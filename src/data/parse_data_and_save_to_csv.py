6a6bef3a59bcd9d769df9171c706e2f2c4f90311
Chudakov Matvey <morganrush@yandex.ru>
2021-05-13 16:23:11 +0300

    added ticker param to data_parser result

--- src/data/parse_data_and_save_to_csv.py
+++ src/data/parse_data_and_save_to_csv.py
@@ -7,7 +7,7 @@ from src.data.data_parser import get_fundamental_data, get_stock_ticker_list
 # Parse data from resource
 from src.data.metrics import all_metrics
 
-sourceFileName = '../data/nasdaq_screener_1620853648876.csv' # Replace on your own file
+sourceFileName = '../../data/nasdaq_screener_1620853648876.csv' # Replace on your own file
 stock_ticker_list = get_stock_ticker_list(sourceFileName, 'Symbol')
 df = pd.DataFrame(index=stock_ticker_list)
 result = get_fundamental_data(df)
@@ -18,12 +18,12 @@ notFound = result[1]
 file = open('../../data/data.csv', 'w')
 writer = csv.writer(file)
 delimiter = ','
-writer.writerow(all_metrics)
+writer.writerow(['TICKER'] + all_metrics)
 
 for ticker in df.index:
     if ticker in notFound:
         continue
-    values = []
+    values = [ticker]
     for metric in all_metrics:
         values.append(df.loc[ticker, metric])
     writer.writerow(values)
